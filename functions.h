d39733eb2bc6cc19175fe81b6ef1491154dd7ab0
vinizika <85229756+vinizika@users.noreply.github.com>
2023-10-17 18:48:43 -0300

    Adição das funções prontas já até o momento, que são referentes às opções 1 e 3.

--- functions.h
+++ functions.h
@@ -1,5 +1,19 @@
+// Vinícius de Castro Duarte - 24.123.073-9
+// Renan Ivaskevicius Vieira - 24.123.069-7
+
 #ifndef UNTITLED_FUNCTIONS_H
 #define UNTITLED_FUNCTIONS_H
 
+char confirmacao(int operacao);
+
+char confirma_input();
+
+char* input_cnpj();
+
+void novo_cliente();
+
+void confirmacao_cnpj();
+
+void listar();
 
 #endif
