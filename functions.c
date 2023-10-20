143d4ea168ad36a66969ccdb48a130a5eb7aecbc
renanivask <95446292+renanivask@users.noreply.github.com>
2023-10-20 18:03:06 -0300

    Adição das funções 4 (Débito) e 8 (Débito Automático)

--- functions.c
+++ functions.c
@@ -55,7 +55,7 @@ char* input_cnpj() {
     return cnpj;
 }
 
-// opcao 1 - novo cliente
+// Opcao 1 - novo cliente
 void novo_cliente() {
     Cliente novo;
     memset(&novo, 0, sizeof(Cliente));
@@ -106,7 +106,7 @@ void novo_cliente() {
     }
 }
 
-// opcao 3 - listar clientes
+// Opcao 3 - listar clientes
 void listar() {
     // Abra o arquivo binário para leitura
     FILE *file = fopen("clients.bin", "rb");
@@ -128,4 +128,172 @@ void listar() {
 
         fclose(file);
     }
+}
+
+// Operacao 4: Debito
+void debito() {
+    char cnpj[15];
+    printf("Digite o CNPJ do cliente: ");
+    scanf("%s", cnpj);
+
+    FILE* file = fopen("clients.bin", "rb+"); // Abra o arquivo em modo de leitura e escrita binária.
+
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+
+    Cliente cliente;
+
+    while (fread(&cliente, sizeof(Cliente), 1, file) == 1) {
+        if (strcmp(cliente.cnpj, cnpj) == 0) {
+            char senha[50];
+            printf("Digite sua senha: ");
+            scanf(" %s", senha);
+
+            while (strcmp(cliente.senha, senha) != 0) {
+                printf("Senha incorreta. Digite novamente sua senha: ");
+                scanf(" %s", senha);
+            }
+
+            double valor_debito;
+            printf("Qual valor você deseja debitar: R$");
+            scanf("%lf", &valor_debito);
+
+            double valor = valor_debito;
+            double taxa = 0.0;
+
+            if (cliente.tipo_conta == 'C') {
+                taxa = valor * 0.05;
+                double novo_saldo = cliente.saldo - (valor + taxa);
+
+                while (novo_saldo < -1000.0) {
+                    printf("Limite de R$1000.00 atingido.\n");
+                    printf("Digite um valor valido: R$");
+                    scanf("%lf", &valor_debito);
+                    valor = valor_debito;
+                    novo_saldo = cliente.saldo - (valor + taxa);
+                }
+
+                cliente.saldo = novo_saldo;
+            } else {
+                taxa = valor * 0.03;
+                double novo_saldo = cliente.saldo - (valor + taxa);
+
+                while (novo_saldo < -5000.0) {
+                    printf("Limite de R$5000.00 atingido.\n");
+                    printf("Digite um valor valido: R$");
+                    scanf("%lf", &valor_debito);
+                    valor = valor_debito;
+                    novo_saldo = cliente.saldo - (valor + taxa);
+                }
+
+                cliente.saldo = novo_saldo;
+            }
+
+            // Atualize as transações do cliente
+            time_t current_time;
+            time(&current_time);
+            struct tm* local_time = localtime(&current_time);
+
+            sprintf(cliente.transacoes[cliente.num_transacoes], "Data: %04d-%02d-%02d %02d:%02d:%02d / -R$%.2lf / Tarifa: R$%.2lf / Saldo: R$%.2lf",
+                    local_time->tm_year + 1900, local_time->tm_mon + 1, local_time->tm_mday,
+                    local_time->tm_hour, local_time->tm_min, local_time->tm_sec, valor, taxa, cliente.saldo);
+
+            // Atualize o número de transações
+            cliente.num_transacoes++;
+
+            // Volte para a posição correta no arquivo
+            fseek(file, -sizeof(Cliente), SEEK_CUR);
+
+            // Escreva o registro atualizado de volta no arquivo binário
+            fwrite(&cliente, sizeof(Cliente), 1, file);
+
+            printf("Saldo de R$%.2lf\n", cliente.saldo);
+            fclose(file);
+            return;
+        }
+    }
+
+    printf("Cliente nao encontrado!\n");
+    fclose(file);
+}
+
+// Operacao 8: Débito Automático
+void debito_automatico() {
+    char cnpj[15];
+    printf("Digite o CNPJ do cliente: ");
+    scanf("%s", cnpj);
+
+    FILE* file = fopen("clients.bin", "rb+"); // Abra o arquivo em modo de leitura e escrita binária.
+
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+
+    Cliente cliente;
+
+    while (fread(&cliente, sizeof(Cliente), 1, file) == 1) {
+        if (strcmp(cliente.cnpj, cnpj) == 0) {
+            char senha[50];
+            printf("Digite sua senha: ");
+            scanf(" %s", senha);
+
+            while (strcmp(cliente.senha, senha) != 0) {
+                printf("Senha incorreta. Digite novamente sua senha: ");
+                scanf(" %s", senha);
+            }
+
+            double valor_debito;
+            printf("Qual o valor do debito automatico: R$");
+            scanf("%lf", &valor_debito);
+
+            int dia;
+            printf("Em qual dia do mes você deseja o debito automatico: ");
+            scanf("%d", &dia);
+
+            if (dia < 1 || dia > 31) {
+                printf("Dia invalido. O dia deve estar entre 1 e 31.\n");
+                fclose(file);
+                return;
+            }
+
+            double valor = valor_debito;
+            double novo_saldo = cliente.saldo - valor;
+
+            while (novo_saldo < -1000.0) {
+                printf("Limite de R$1000.00 atingido.\n");
+                printf("Digite um valor valido: R$");
+                scanf("%lf", &valor_debito);
+                valor = valor_debito;
+                novo_saldo = cliente.saldo - valor;
+            }
+
+            // Obtenha a data e hora atual
+            time_t current_time;
+            time(&current_time);
+            struct tm* local_time = localtime(&current_time);
+
+            // Agende o débito automático
+            sprintf(cliente.transacoes[cliente.num_transacoes], "Debito automatico agendado para o dia %d/%02d/%04d / -R$%.2lf / Tarifa: R$0.00 / Saldo: R$%.2lf",
+                    dia, local_time->tm_mon + 1, local_time->tm_year + 1900, valor, novo_saldo);
+
+            cliente.num_transacoes++;
+            cliente.saldo = novo_saldo;
+
+            // Volte para a posição correta no arquivo
+            fseek(file, -sizeof(Cliente), SEEK_CUR);
+
+            // Escreva o registro atualizado de volta no arquivo binário
+            fwrite(&cliente, sizeof(Cliente), 1, file);
+
+            printf("Debito automatico agendado com sucesso!\n");
+            fclose(file);
+            return;
+        }
+    }
+
+    printf("Cliente nao encontrado!\n");
+    fclose(file);
 }
